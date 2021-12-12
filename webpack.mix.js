3ad41c85ebd69a9aea3f8728d99601e7aa3320e6
BariqDharmawan <bariq.2nd.rodriguez@gmail.com>
2021-12-12 20:17:38 +0700

    [ADD] all

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,7 @@
 let mix = require('laravel-mix');
 
 mix.setPublicPath('dist')
-    .js('src/js/app.js', 'dist')
-    .js('src/js/vendor.js', 'dist')
+    .js('src/js/app.js', 'dist/js')
+    .js('src/js/vendor.js', 'dist/js')
     .copy('src/img', 'dist/img')
-    .sass('src/sass/app.scss', 'dist')
+    .sass('src/sass/app.scss', 'dist/css')
