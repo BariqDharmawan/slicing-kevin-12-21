2678f740aa84fb6a051fc0b007285527255a9a7c
BariqDharmawan <bariq.2nd.rodriguez@gmail.com>
2021-12-22 11:00:42 +0700

    [UPDATE] dist css

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,6 @@
 let mix = require('laravel-mix');
 
 mix.setPublicPath('dist')
-    .js('src/js/app.js', 'dist/js')
     .js('src/js/vendor.js', 'dist/js')
     .copy('src/img', 'dist/img')
     .sass('src/sass/app.scss', 'dist/css')
